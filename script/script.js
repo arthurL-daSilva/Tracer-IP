ed98984a7ce7f92df1a4d06d4599b6324823d55e
arthurL-daSilva <arthurleal723@gmail.com>
2024-09-03 01:17:52 -0300

    Adicionado mais elementos

--- script/script.js
+++ script/script.js
@@ -1,45 +1,35 @@
 //https://apiip.net/user/dashboard
 
 function limitedInput(){
-    //Pega o conteudo com id "input_ip"
     var ip = document.getElementById("input_ip");
-
-    // Armazena o conteudo do id "input_ip" na váriável ip_input
     var ip_content = ip.value;
-    // mostra o conteudo da variavel ip_content
 
     if(ip_content.length > 12){
-        //ip_content.slice() - extrai uma parte 
-        //da string com base nos índices específicados
-        //de 0 a 15.
         ip.value = ip_content.slice(0, 15);
     }
     console.log(ip_content)
 }
 
 function InputButtonConteudo(){
-    var color = document.getElementById("changeColor");
-    var position =  document.getElementById("changePosition");
-    color.style.backgroundColor = "#DCDCDC";
-    position.style.alignItems = "flex-start";
-    position.style.justifyContent = "flex-start";
+    var input_box = document.getElementById("changeProperties");
+    var request_box = document.getElementById("API_request");
+    input_box.style.backgroundColor = "#DCDCDC";
+    input_box.style.transform = "translateY(0)";
+    input_box.style.transition = "2s ease-out";
+    request_box.style.transitionDuration = "2s";
+    request_box.style.transitionDelay = "1.3s";
+    request_box.style.opacity = "1";
+    request_box.style.transform = "translateY(0)";
 }
 
 async function getIpInfo (){
-    // Set endpoint and your access key
     const ip = document.getElementById("input_ip");
     const ip_content = ip.value;
     const accessKey = '5b7eb238-810e-4b9d-b14c-8421ca4f52d8';
     const url = 'https://apiip.net/api/check?ip='+ ip_content +'&accessKey='+ accessKey; 
-  
-    // Make a request and store the response
     const response = await fetch(url);
-  
-    // Decode JSON response:
     const result = await response.json();
   
-    // Output the "code" value inside "currency" object
     console.log(result);
     InputButtonConteudo();
-  };
-  getIpInfo();
+}
