138e6a1893cc84fe36669563eb0283c176cf7dce
arthurL-daSilva <arthurleal723@gmail.com>
2024-09-03 15:18:21 -0300

    Começado a fazer a inserção dos elementos da API no HTML com o JavaScript

--- script/script.js
+++ script/script.js
@@ -7,19 +7,26 @@ function limitedInput(){
     if(ip_content.length > 12){
         ip.value = ip_content.slice(0, 15);
     }
-    console.log(ip_content)
+    console.log(ip_content);
 }
 
-function InputButtonConteudo(){
+function InputButtonConteudo(callback){
+    var input_error = document.getElementById("ip_notfound");
     var input_box = document.getElementById("changeProperties");
     var request_box = document.getElementById("API_request");
-    input_box.style.backgroundColor = "#DCDCDC";
-    input_box.style.transform = "translateY(0)";
-    input_box.style.transition = "2s ease-out";
-    request_box.style.transitionDuration = "2s";
-    request_box.style.transitionDelay = "1.3s";
-    request_box.style.opacity = "1";
-    request_box.style.transform = "translateY(0)";
+    if(callback){
+        input_error.style.opacity = "0";
+        input_box.style.backgroundColor = "#DCDCDC";
+        input_box.style.transform = "translateY(0)";
+        input_box.style.transition = "ease 2s";
+        request_box.style.transitionDuration = "2s";
+        request_box.style.transitionDelay = "1.3s";
+        request_box.style.opacity = "1";
+        request_box.style.transform = "translateY(0)";
+    }
+    else{
+        input_error.style.opacity = "1";
+    }
 }
 
 async function getIpInfo (){
@@ -28,8 +35,15 @@ async function getIpInfo (){
     const accessKey = '5b7eb238-810e-4b9d-b14c-8421ca4f52d8';
     const url = 'https://apiip.net/api/check?ip='+ ip_content +'&accessKey='+ accessKey; 
     const response = await fetch(url);
-    const result = await response.json();
-  
-    console.log(result);
-    InputButtonConteudo();
+    if(response.status == 400){
+        console.log("Error!");
+        InputButtonConteudo(false);
+    }else{
+        const result = await response.json();
+        console.log(result);
+        InputButtonConteudo(true);
+        //ELEMENTOS DA API
+        let cidade = document.getElementById("cidade")
+        cidade.innerHTML = result.city;
+    }
 }
